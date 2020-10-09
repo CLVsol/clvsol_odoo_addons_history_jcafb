2bbc659c5440b9e171512b13925c331e384b1051
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-10-09 12:02:46 -0300

    [REM] clv_address_history_jcafb (Migrated to repository "clvsol_odoo_addons_jcafb")

--- clv_address_history_jcafb/wizard/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import address_history_updt
-from . import address_document_setup
-from . import address_lab_test_request_setup
